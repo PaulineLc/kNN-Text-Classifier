59d6accc4b6ccae16693a3d369b084c3315441da
Pauline Lallinec <PaulineLc@users.noreply.github.com>
2016-11-06 15:11:03 +0000

    redesigned create_bag_of_words; tested it (OK)

--- runner.py
+++ runner.py
@@ -2,6 +2,7 @@ import pandas as pd
 import math
 import operator
 
+# Todo: redesign classifier class
 # Todo: better design for classes
 # Todo: puts the methods where they belong e.g. get_category in the document
 # Todo: Maybe have Document inherit from TextData
@@ -14,9 +15,10 @@ from Assignment.classifier import TextClassifier
 
 TextData.define_article_data(data_file='data/news_articles.mtx')
 TextData.define_article_labels('data/news_articles.labels')
-my_document = Document(500)
+my_document = Document(666)
 
 print(my_document.get_category())
+print(my_document.create_bag_of_words())
 
 # clf = TextClassifier(my_document, my_dataset)
 # clf.create_similarity_dic()
--- text_data.py
+++ text_data.py
@@ -9,11 +9,12 @@ class Document(TextData):
 
     def __init__(self, doc_id):
         self.doc_id = doc_id
-        self.bag_of_words = {}
+        self.bag_of_words = {} #Those will be set when we need them
+        self.label = '' #no need for them till we need them - lazy approach
 
-    def create_bag_of_words(self, dataset):
+    def create_bag_of_words(self):
         '''returns a dictionary of all (term_id, occurrences) of the terms present in the document'''
-        df = dataset.df.loc[dataset.df['doc_id'] == self.doc_id].reset_index()
+        df = TextData.article_data.loc[TextData.article_data['doc_id'] == self.doc_id].reset_index()
         for i in range(df.shape[0]):
             self.bag_of_words[df['term_id'][i]] = df['nb_occurences'][i]
         return self.bag_of_words
@@ -24,5 +25,6 @@ class Document(TextData):
             return ""
 
         doc_index = TextData.article_labels['class'].loc[TextData.article_labels['doc_id'] == self.doc_id].index[0]
-        doc_class = TextData.article_labels['class'][doc_index]
-        return doc_class
+        label = TextData.article_labels['class'][doc_index]
+        self.label = label
+        return label
